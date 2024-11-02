e9fc1080cc3e9137926a7a0c6196986c3012c944
VeraDev0 <veradev0@proton.me>
2024-11-02 19:33:02 +0100

    Fix #2

--- ru/assets/js/acecf23e.a1d55985.js
+++ ru/assets/js/acecf23e.a1d55985.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunkscrap_computers_docs=self.webpackChunkscrap_computers_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/ru/blog","blogTitle":"Blog","authorsListPath":"/ru/blog/authors"}')}}]);
+"use strict";(self.webpackChunkscrap_computers_docs=self.webpackChunkscrap_computers_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/SCDocs/ru/blog","blogTitle":"Blog","authorsListPath":"/SCDocs/ru/blog/authors"}')}}]);
