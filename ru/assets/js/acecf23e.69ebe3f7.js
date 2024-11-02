fca513be1a22de73cc305f0cfc8f40883aa60476
VeraDev0 <veradev0@proton.me>
2024-11-02 20:22:21 +0100

    Fix the site once for all

--- ru/assets/js/acecf23e.69ebe3f7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkscrap_computers_docs=self.webpackChunkscrap_computers_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/SCDocs/ru/blog","blogTitle":"Blog"}')}}]);
