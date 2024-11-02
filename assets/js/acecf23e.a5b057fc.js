3438bf3f56c873abc93273b314142fd361647bca
VeraDev0 <veradev0@proton.me>
2024-11-02 19:37:02 +0100

    Fix Blog urls

--- assets/js/acecf23e.a5b057fc.js
+++ assets/js/acecf23e.a5b057fc.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunkscrap_computers_docs=self.webpackChunkscrap_computers_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog"}')}}]);
+"use strict";(self.webpackChunkscrap_computers_docs=self.webpackChunkscrap_computers_docs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/SCDocs/blog","blogTitle":"Blog"}')}}]);
