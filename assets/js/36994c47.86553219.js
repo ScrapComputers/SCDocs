30f1538f9fa60bc586a7755e76e92cf91d643881
VeraDev <159570832+VeraDev0@users.noreply.github.com>
2026-01-18 10:49:21 +0100

    Fix sc.fontmanager (now sc.font) and update annoucement bar

--- assets/js/36994c47.86553219.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkscrap_computers_docs=self.webpackChunkscrap_computers_docs||[]).push([[9858],{5516:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
